6a801744359070499f0d714ffd8753c9a6d8f5a6
Aleksey Druzhinin <alexey@yourtask.ru>
2020-10-26 09:42:24 +0500

    files

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12
+    },
+    "plugins": [],
+    "rules": {
+    }
+};
